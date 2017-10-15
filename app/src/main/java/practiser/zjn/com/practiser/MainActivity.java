e2120fa1a74f8d9766c4d35ccde5ed5592e4a767
zhangjianan <zhangjianan@csii.com.cn>
2017-10-15 10:10:27 +0800

    first commit

---
+++ app/src/main/java/practiser/zjn/com/practiser/MainActivity.java
@@ -0,0 +1,13 @@
+package practiser.zjn.com.practiser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
