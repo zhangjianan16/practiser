2761134f61e4ec39ca32abfc3109ddc591937a21
zhangjianan <zhangjianan@csii.com.cn>
2017-10-17 20:06:59 +0800

    添加侧滑栏drawerlayout

--- app/src/main/java/com/zjn/practiser/MainActivity.java
+++ app/src/main/java/com/zjn/practiser/MainActivity.java
@@ -2,13 +2,16 @@ package com.zjn.practiser;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+import android.view.Window;
 
 import com.zjn.practiser.adapter.MainRecycleViewAdapter;
 import com.zjn.practiser.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public ActivityMainBinding binding;
 
@@ -16,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // 继承AppCompatActivity去掉titlebar的方法
-        // supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         init();
     }
@@ -35,5 +38,22 @@ public class MainActivity extends AppCompatActivity {
         // binding.recyclerview.setLayoutManager(new StaggeredGridLayoutManager(3,StaggeredGridLayoutManager.VERTICAL));
 
         binding.recyclerview.setAdapter(mainRecycleViewAdapter);
+        binding.ivLiftTouch.setOnClickListener(this);
+        binding.ivHeader.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.iv_lift_touch:
+                //打开左侧侧滑栏
+                binding.drawerlayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.iv_header:
+                //关闭左侧侧滑栏
+                binding.drawerlayout.closeDrawer(GravityCompat.START);
+                break;
+            default:
+        }
     }
 }
